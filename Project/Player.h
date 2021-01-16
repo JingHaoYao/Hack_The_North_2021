6b3b4e75aa88b98cae47186d4dcc299a28ef8633
Karthik002 <karthikprasad62@gmail.com>
2021-01-15 20:08:53 -0500

    added player.h and player.cpp

---
+++ Project/Player.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "GameEngine/EntitySystem/Entity.h"
+#include "GameEngine/EntitySystem/Components/SpriteRenderComponent.h"
+#include "GameEngine/EntitySystem/Components/CollidableComponent.h"
+#include "GameEngine/EntitySystem/Components/SoundComponent.h"
+#include "GameEngine/Util/TextureManager.h"
+
+namespace Game {
+
+	class Player : public GameEngine::Entity
+	{
+	public:
+		Player(GameEngine::eTexture::type);
+
+		~Player();
+
+	protected:
+		GameEngine::SpriteRenderComponent* spriteRenderComponent;
+		GameEngine::CollidableComponent* collidableComponent;
+		GameEngine::SoundComponent* soundComponent;
+		void Update();
+	};
+
+}
+
+
